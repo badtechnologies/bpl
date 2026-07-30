eb3ef7d1fd026efbd57a220c3c0c5519ef65cebe
Logan Dhillon <dev@logandhillon.com>
2026-07-29 21:09:10 -0400

    fix(bssh): simplify setup script to work on bpm

--- lib/bssh/setup.py
+++ lib/bssh/setup.py
@@ -1,6 +1,5 @@
 from bdsh.shell import Shell
 from paramiko import RSAKey
 
-if __name__ == '__main__':
-    key = RSAKey.generate(bits=2048)
-    key.write_private_key_file(Shell.get_path('cfg', 'badbandssh_rsa_key'))
+key = RSAKey.generate(bits=2048)
+key.write_private_key_file(Shell.get_path('cfg', 'badbandssh_rsa_key'))
